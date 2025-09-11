0c53f44dbf3b76171fe6cd1e2b4871c665262eb4
kyron <jameskyronsostino36@gmail.com>
2025-09-11 11:07:14 +0800

    table

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -10,54 +10,51 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        if (!Schema::hasTable('ldrJobs')) {
-            Schema::create('ldrJobs', function (Blueprint $table) {
-                $table->id();
-                $table->string('queue')->index();
-                $table->longText('payload');
-                $table->unsignedTinyInteger('attempts');
-                $table->unsignedInteger('reserved_at')->nullable();
-                $table->unsignedInteger('available_at');
-                $table->unsignedInteger('created_at');
-            });
-        }
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
 
-        if (!Schema::hasTable('ldrJob_batches')) {
-            Schema::create('ldrJob_batches', function (Blueprint $table) {
-                $table->string('id')->primary();
-                $table->string('name');
-                $table->integer('total_jobs');
-                $table->integer('pending_jobs');
-                $table->integer('failed_jobs');
-                $table->longText('failed_job_ids');
-                $table->mediumText('options')->nullable();
-                $table->integer('cancelled_at')->nullable();
-                $table->integer('created_at');
-                $table->integer('finished_at')->nullable();
-            });
-        }
+        Schema::create('job_batches', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->integer('total_jobs');
+            $table->integer('pending_jobs');
+            $table->integer('failed_jobs');
+            $table->longText('failed_job_ids');
+            $table->mediumText('options')->nullable();
+            $table->integer('cancelled_at')->nullable();
+            $table->integer('created_at');
+            $table->integer('finished_at')->nullable();
+        });
 
-        if (!Schema::hasTable('ldrFailed_jobs')) {
-            Schema::create('ldrFailed_jobs', function (Blueprint $table) {
-                $table->id();
-                $table->string('uuid')->unique();
-                $table->text('connection');
-                $table->text('queue');
-                $table->longText('payload');
-                $table->longText('exception');
-                $table->timestamp('failed_at')->useCurrent();
-            });
-        }
-
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
     }
 
     /**
      * Reverse the migrations.
      */
+
     public function down(): void
     {
         Schema::dropIfExists('ldrJobs');
         Schema::dropIfExists('ldrJob_batches');
         Schema::dropIfExists('ldrFailed_jobs');
+        Schema::dropIfExists('job_batches');
+        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('failed_jobs');
     }
 };
